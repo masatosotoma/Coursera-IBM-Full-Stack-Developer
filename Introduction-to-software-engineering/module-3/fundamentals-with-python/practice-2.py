9a1ca1f352672aaeea241aa81d0e33890f9c6c59
masatosotoma <masatosotoma@gmail.com>
2024-09-12 10:19:57 -0400

    Python exercises

---
+++ Introduction-to-software-engineering/module-3/fundamentals-with-python/practice-2.py
@@ -0,0 +1,23 @@
+score =88
+
+if score > 90:
+    print("Well done! That gets you an A+ grade")
+elif score >= 70:
+    print("Good work! Passed the test!")
+else:
+    print("Better luck next time!")
+
+myval = 8
+if myval > 0:
+    print("It is a positive number")
+else:
+    print("It is a negative number")
+
+
+mylist = ["John","Sam","Adam"]
+for x in mylist:
+    print(x)
+
+myArray = [20,10,44,928,200]
+for y in myArray:
+    print(y)

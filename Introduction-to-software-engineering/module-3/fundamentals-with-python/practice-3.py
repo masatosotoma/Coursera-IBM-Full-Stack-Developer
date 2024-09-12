34ddb72dc622d669aa9ec9cf2cc35a6316754851
masatosotoma <masatosotoma@gmail.com>
2024-09-12 10:36:38 -0400

    Python basic practices

---
+++ Introduction-to-software-engineering/module-3/fundamentals-with-python/practice-3.py
@@ -0,0 +1,35 @@
+mylist = ["computer","monitor","table"]
+for x in mylist:
+    print(x)
+    if x == "monitor":
+        break
+
+shopping_list=["bread","water","chips","candy","soda"]
+for y in shopping_list:
+    print(y)
+    if y == "chips":
+        break
+
+A = 1
+while A < 12:
+    print(A)
+    A +=2
+
+score = 60
+while score < 90:
+    print(score)
+    score += 5
+
+B = 1
+while B < 36:
+    print(B)
+    if B < 20:
+        break
+    B += 6
+
+myval = 45
+while myval < 90:
+    print(myval)
+    if myval >80:
+        break
+    myval += 8

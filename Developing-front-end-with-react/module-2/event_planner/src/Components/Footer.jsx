1dc495ab7c36b67d8a88a09b26263fda71e0d59d
MasatoSotoma <masatosotoma@gmail.com>
2024-10-24 22:57:54 -0400

    Completed Func Component page practice

---
+++ Developing-front-end-with-react/module-2/event_planner/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <>
+        <p>&copy; Event Planner Organization. All rights reserved.</p>
+      </>
+    );
+}
+
+export default Footer;

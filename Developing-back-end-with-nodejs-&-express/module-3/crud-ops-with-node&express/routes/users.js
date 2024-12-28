4e420749382c011d37cdfdbea9a530e3f64e0b6d
MasatoSotoma <masatosotoma@gmail.com>
2024-12-28 00:01:44 -0500

    Updated get email method

--- Developing-back-end-with-nodejs-&-express/module-3/crud-ops-with-node&express/routes/users.js
+++ Developing-back-end-with-nodejs-&-express/module-3/crud-ops-with-node&express/routes/users.js
@@ -36,16 +36,26 @@ router.get("/", (req, res) => {
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  // Extract the email parameter from the request URL
+  const email = req.params.email;
+  // Filter the users array to find users whose email matches the extracted email parameter
+  let filtered_users = users.filter((user) => user.email === email);
+  // Send the filtered_users array as the response to the client
+  res.send(filtered_users);
 });
 
 // POST request: Create a new user
 router.post("/", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  // Push a new user object into the users array based on query parameters from the request
+  users.push({
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    email: req.query.email,
+    DOB: req.query.DOB,
+  });
+  // Send a success message as the response, indicating the user has been added
+  res.send("The user " + req.query.firstName + " has been added!");
 });
-
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
   // Extract email parameter and find users with matching email

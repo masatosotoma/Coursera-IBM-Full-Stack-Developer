e056cd6652ed3f3ebd2bad24c8a4906c83ae5ff7
MasatoSotoma <masatosotoma@gmail.com>
2025-04-01 21:46:17 -0400

    Python data type practice

---
+++ Python-for-data-science/module-1/hands-on-exercise-2.py
@@ -0,0 +1,31 @@
+print(type("Hello, Python 101!"))
+
+print(type(12.0))
+
+print(type(float(2)))
+
+print(type(0.56))
+
+print(int(1.1))
+
+# print(int('1 or 2 people'))  error
+
+print(float('1.2'))
+
+print(str(1))
+print(str(1.2))
+
+print(type(True))
+print(type(False))
+
+print(int(True))
+print(int(False))
+
+print(type(6/2))
+print(type(6//2))
+
+print(type("Hello, World"))
+print(type("hello" == "world"))
+
+print(type(float("12345.56")))
+print(type(str("123-456-7890")))

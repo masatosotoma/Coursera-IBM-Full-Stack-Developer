41b4c992d4d181f63c38af569b58578d107cd8d8
MasatoSotoma <masatosotoma@gmail.com>
2025-04-02 23:55:44 -0400

    Python Exercise 3

---
+++ Python-for-data-science/module-1/hands-on-exercise-3.py
@@ -0,0 +1,21 @@
+
+x = 43+60+16+41
+print(x)
+
+y = x/60
+
+print(y)
+
+# Write an expression to add 30 and 20 and subtract 40
+print(30+20-40)
+
+# Write an expression to subtract 5 from 55 and divide the result by 10
+print(
+    (55-5)/10)
+
+a = 3 + 2 * 2
+print(a)
+b = (3 + 2) * 2
+print(b)
+c = a+b
+print(c)

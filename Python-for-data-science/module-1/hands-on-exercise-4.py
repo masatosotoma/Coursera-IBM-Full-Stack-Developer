8eefbd14a6c41481ccbb8558d075886575c41c28
MasatoSotoma <masatosotoma@gmail.com>
2025-04-03 23:40:37 -0400

    Python string exercise

---
+++ Python-for-data-science/module-1/hands-on-exercise-4.py
@@ -0,0 +1,13 @@
+name = "The BodyGuard"
+name
+
+print(name[0]) #it's T
+print(name[6]) #it's o
+
+print(name[0:4])
+print(name[0:5:2])
+
+print(" The BodyGuard \t is the best album" )
+
+print(" The BodyGuard \\ is the best album")
+
